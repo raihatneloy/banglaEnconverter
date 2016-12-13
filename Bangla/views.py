58e148f554e66b0b264fcc98d339f52934205b9c
raihatneloy <raihat.zaman@newscred.com>
2016-12-13 21:47:20 +0600

    Encorvert to English

--- Bangla/views.py
+++ Bangla/views.py
@@ -7,14 +7,22 @@ def index(request):
     return render(request, "home.html")
 
 def submit(request):
-    language = request.POST.get('lang')
+    language1 = request.POST.get('lang1')
+    language2 = request.POST.get('lang2')
+    language3 = request.POST.get('lang3')
     enOrde = request.POST.get('enordn')
-    text = request.POST.get('inputText')
+    text1 = request.POST.get('inputText1')
+    text2 = request.POST.get('inputText2')
+    text3 = request.POST.get('inputText3')
 
+    print language1,language2,language3
+    print text1, text2, text3
 
-    if language == "English":
+    if language2 == "English":
         if enOrde == "Encode":
-            x = "DOMAIN=SPORT&password=guest&TAGERROR=NO&username=UNL_guest&conversion=true&language=en&data=%s&outputmode=text&coding=utf-8" % (text)
+            x = "DOMAIN=SPORT&password=guest&TAGERROR=NO&username=UNL_guest&conversion=true&language=en&data=%s&outputmode=text&coding=utf-8" % (text2)
+            print x
+
             r = requests.post('http://unl.ru/etap-cgi/cgiunl.exe', data=x)
             print r.text
             ans = ''
@@ -25,11 +33,13 @@ def submit(request):
                 else:
                     ans += r.text[i]
 
+            print ans
+
             return render(request, "submit.html",{'rule' : HttpResponse(r.text)})
 
         else:
             x = "DOMAIN=SPORT&password=guest&TAGERROR=NO&username=UNL_guest&conversion=false&language=en&data=%s&outputmode=text&coding=utf-8" % (
-            text)
+            text2)
             r = requests.post('http://unl.ru/etap-cgi/cgiunl.exe', data=x)
 
-            return HttpResponse(r.text)
+            return render(request, "submit.html",{'rule' : HttpResponse(r.text)})
